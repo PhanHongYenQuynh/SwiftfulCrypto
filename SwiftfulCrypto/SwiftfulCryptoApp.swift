2f096abf56ed6e15cb94c5f177e3bfb5794b1bf9
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-09-20 13:17:36 +0700

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Phan Hong Yen Quynh on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
