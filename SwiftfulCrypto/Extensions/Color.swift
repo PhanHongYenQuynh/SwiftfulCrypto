33aa3b9e4d06636b9ba142a84d92e1b1aea98ad3
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-09-20 13:35:08 +0700

    Color Theme

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Phan Hong Yen Quynh on 20/09/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme{
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
