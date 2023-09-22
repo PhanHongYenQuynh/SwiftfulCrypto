b6a797897affac524d60cc2a10216b775365ef7b
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-09-22 19:33:34 +0700

    Saving images to the File Manager

--- SwiftfulCrypto/Services/CoinImageService.swift
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -15,20 +15,37 @@ class CoinImageService{
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let filManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel){
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage(){
+        if let savedImage = filManager.getImage(imageName: imageName, folderName: folderName){
+            image = savedImage
+        }else{
+            downloadCoinImage()
+        }
+    }
+    
+    
+    private func downloadCoinImage(){
+        
+        
         guard let url = URL(string: coin.image) else {return}
         
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in return UIImage(data: data)})
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else{return}
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.filManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
     
---
+++ SwiftfulCrypto/Utilities/LocalFilleManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocalFilleManager.swift
+//  SwiftfulCrypto
+//
+//  Created by Phan Hong Yen Quynh on 22/09/2023.
+//
+
+import Foundation
