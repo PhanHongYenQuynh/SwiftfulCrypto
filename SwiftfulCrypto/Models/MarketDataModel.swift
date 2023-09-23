93c4e51b7e5a6e5249ed3c3ec6b4f8a324d4779f
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-09-23 22:10:37 +0700

    fixed

--- SwiftfulCrypto/Models/MarketDataModel.swift
+++ SwiftfulCrypto/Models/MarketDataModel.swift
@@ -173,7 +173,7 @@ struct MarketDataModel: Codable {
     let marketCapChangePercentage24HUsd: Double
    
     enum CodingKeys: String, CodingKey{
-        case totalMarketCap = "total_marker_cap"
+        case totalMarketCap = "total_market_cap"
         case totalVolume = "total_volume"
         case marketCapPercentage = "market_cap_percentage"
         case marketCapChangePercentage24HUsd = "market_cap_change_percentage_24h_usd"
