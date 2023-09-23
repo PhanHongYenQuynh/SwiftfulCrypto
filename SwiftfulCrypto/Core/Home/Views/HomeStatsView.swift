60cda3924bfd3490173af2e131ea97323092272a
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-09-23 22:05:32 +0700

    Download and display live market data from API

--- SwiftfulCrypto/Core/Home/Views/HomeStatsView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeStatsView.swift
@@ -25,7 +25,7 @@ struct HomeStatsView: View {
 }
 
 struct HomeStatsView_Previews: PreviewProvider {
-    static var previews: some View{
+    static var previews: some View {
         HomeStatsView(showPortfolio: .constant(false))
             .environmentObject(dev.homeVM)
     }
