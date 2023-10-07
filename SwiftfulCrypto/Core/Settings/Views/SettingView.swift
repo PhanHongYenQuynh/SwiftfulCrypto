5b455c3462c6b7700ed2dd6de2a0af412f31104e
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-10-07 20:54:14 +0700

    Update LifeStyle

--- SwiftfulCrypto/Core/Settings/Views/SettingView.swift
+++ SwiftfulCrypto/Core/Settings/Views/SettingView.swift
@@ -63,6 +63,7 @@ struct SettingView: View {
                 signout
             
             }
+            .listStyle(SidebarListStyle())
             .font(.headline)
             .accentColor(.accent)
             .navigationTitle("Settings")
