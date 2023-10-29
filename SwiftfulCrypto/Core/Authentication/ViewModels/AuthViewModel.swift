de43dca65a1e1448e551c421bd1cbf84911835f6
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-10-30 00:43:05 +0700

    SignIn with AppleID

--- SwiftfulCrypto/Core/Authentication/ViewModels/AuthViewModel.swift
+++ SwiftfulCrypto/Core/Authentication/ViewModels/AuthViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 import GoogleSignIn
+import AuthenticationServices
+import CryptoKit
 
 protocol AuthenticationFormProtocol{
     var formIsValid: Bool {get}
@@ -25,7 +27,9 @@ class AuthViewModel: ObservableObject{
     @Published var signInError: Error?
     @Published var signUpError: Error?
     @Published var userID: String?
-    static let shared = AuthViewModel() 
+    @Published var errorMessage = ""
+    private var currentNonce: String?
+    static let shared = AuthViewModel()
     init() {
         self.userSession = Auth.auth().currentUser
         Task {
@@ -33,6 +37,7 @@ class AuthViewModel: ObservableObject{
         }
     }
     
+    // MARK: - PUBLIC
     func signIn(withEmail email: String, password: String) async throws{
         do{
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -142,8 +147,106 @@ class AuthViewModel: ObservableObject{
         return false
     }
     
+    func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.fullName, .email]
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.nonce = sha256(nonce)
+      }
+    
+    func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>) {
+        if case .failure(let failure) = result {
+            errorMessage = failure.localizedDescription
+        }
+        else if case .success(let authorization) = result {
+            if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+                guard let nonce = currentNonce else {
+                    fatalError("Invalid state: a login callback was received, but no login request was sent.")
+                }
+                guard let appleIDToken = appleIDCredential.identityToken else {
+                    print("Unable to fetdch identify token.")
+                    return
+                }
+                guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                    print("Unable to serialise token string from data: \(appleIDToken.debugDescription)")
+                    return
+                }
+                
+                let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                          idToken: idTokenString,
+                                                          rawNonce: nonce)
+                Task {
+                    do {
+                        let result = try await Auth.auth().signIn(with: credential)
+                        
+                        // Update userSession
+                        self.userSession = result.user
+                        
+                        // Update currentUser
+                        let currentUser = User(id: result.user.uid, fullname: result.user.displayName ?? "Unknown", email: result.user.email ?? "Unknown")
+                        self.currentUser = currentUser
+
+                        // Add or update user in Firestore
+                        do {
+                            try await Firestore.firestore().collection("user").document(result.user.uid).setData(["fullname": currentUser.fullname, "email": currentUser.email, "id": result.user.uid])
+                        } catch {
+                            print("Error saving data to Firestore: \(error.localizedDescription)")
+                        }
+
+                    } catch {
+                        print("Error authenticating: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - PRIVATE
+    
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
 }
 
 
-//try await Firestore.firestore().collection("user").document(user.id).setData(encodedUser)
-//await fetchUser()
