580222f50870442cb95e3a98715724edbb1c4edd
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-12-15 00:25:07 +0700

    Widget

---
+++ CryptoWidget/CryptoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoWidgetBundle.swift
+//  CryptoWidget
+//
+//  Created by Phan Hong Yen Quynh on 14/12/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CryptoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CryptoWidget()
+    }
+}
